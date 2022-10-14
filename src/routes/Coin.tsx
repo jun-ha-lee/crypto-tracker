002961b67db8015e872b380e227199e4aff05889
jun-ha-lee <hello.git.oil_bird@naver.com>
2022-10-14 17:51:20 +0900

    리액트-타입스크립트

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,6 @@
 // 코인아이디를 클릭하면 그 코인에 관한정보 보이기
 
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"; // 주소의 coinId를 가져온다 (localhost:3000/아이디)
 
 function Coin() {
   // const { coinId } = useParams<"coinId">(); 이렇게 써도 됨
